1c82063b8798b21cd1f3b0a6ac1b822cecc4ef0b
Vili Sinervä <vili.sinerva@helsinki.fi>
2022-11-25 18:19:53 +0200

    Fix pylint errors. Move db files into separate directory

--- src/app.py
+++ src/app.py
@@ -1,8 +1,17 @@
+"""
+Main module for flask app
+"""
 from flask import Flask
-from flask import render_template, redirect, url_for
+#Will be needed in future
+#from flask import render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-        return '<h1> Flask </h1>'
+    """
+    Main page
+    No parameters
+    Returns: html for front page (for use by flask)
+    """
+    return '<h1> Flask </h1>'
--- src/db/init_db.py
+++ src/db/init_db.py
@@ -7,5 +7,5 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
-with open('schema.sql') as schema:
+with open('schema.sql', encoding='utf-8') as schema:
     connection.executescript(schema.read())
