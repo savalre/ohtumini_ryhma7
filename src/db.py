42db9fe361cad9d105826b987e4aaf64334be0db
Mikko Anttonen <mikkohj.anttonen@gmail.com>
2022-11-25 15:23:09 +0200

    Add pylint to poetry + linting

--- src/db.py
+++ src/db.py
@@ -1,3 +1,6 @@
+"""
+Muodostaa yhteyden tietokannan ja flask-appin välille.
+"""
 from os import path
 from flask_sqlalchemy import SQLAlchemy
 from app import app
@@ -6,4 +9,3 @@ basedir = path.abspath(path.dirname(__file__))
 
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + path.join(basedir, 'database.db')
 db = SQLAlchemy(app)
-
--- src/init_db.py
+++ src/init_db.py
@@ -1,7 +1,11 @@
+"""
+Luo tietokannan samassa hakemistossa olevan schema.sql
+tiedoston perusteella.
+"""
 import sqlite3
+
 
 connection = sqlite3.connect('database.db')
 
 with open('schema.sql') as schema:
     connection.executescript(schema.read())
-
