f0ea46389e5fe3791b99fbb33694717e4d95c988
kodtld <kodu@live.fi>
2022-11-27 16:42:47 +0200

    added optional fields for book type citation

--- src/forms/citation_form.py
+++ src/forms/citation_form.py
@@ -13,3 +13,20 @@ class BookCitationForm(FlaskForm):
         render_kw={'placeholder': 'Year...'})
     publisher = StringField('publisher', validators=[InputRequired(), Length(min=4)],
         render_kw={'placeholder': 'Publisher...'})
+
+    volume = IntegerField('volume', validators=[Optional()],
+        render_kw={'placeholder': 'Volume...'})
+    series = IntegerField('series', validators=[Optional()],
+        render_kw={'placeholder': 'Series...'})
+    address = StringField('address', validators=[Optional()],
+        render_kw={'placeholder': 'Address...'})
+    edition = StringField('edition', validators=[Optional()],
+        render_kw={'placeholder': 'Edition...'})
+    month = StringField('month', validators=[Optional()],
+        render_kw={'placeholder': 'Month...'})
+    note = StringField('note', validators=[Optional()],
+        render_kw={'placeholder': 'Note...'})
+    key = StringField('key', validators=[Optional()],
+        render_kw={'placeholder': 'Key...'})
+    url = StringField('url', validators=[Optional()],
+        render_kw={'placeholder': 'Url...'})
