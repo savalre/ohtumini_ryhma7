d70f5749437e71086b7f141ce84a9731abecfc88
Owlaboy <andrei.preotesa.ap@gmail.com>
2022-11-27 19:49:14 +0200

    Removed the complicated old CitationTemplate and Bookcitation classes, replaced witha simpler class, Citation

--- src/citation_class/citation.py
+++ src/citation_class/citation.py
@@ -1,5 +1,12 @@
 class Citation:
     def __init___(self, cite_as, entryname, fieldtypes):
+        """Class for saving the cite_as, entry name and field data,values list
+
+        Args:
+            cite_as (string): Unique tag for the citation, for citing in text
+            entryname (string): Name of the entry
+            fieldtypes (List): List of field type and value pairs in tuples
+        """
         self.cite_as = cite_as
         self.entryname = entryname
         self.fieldtypes = fieldtypes
